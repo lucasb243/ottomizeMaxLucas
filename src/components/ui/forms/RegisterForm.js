de46a4d8ecde823b176c354a0c8ef56050b91d3b
maxetr <73880247+maxetr@users.noreply.github.com>
2022-03-16 09:04:11 +0100

    Merge branch 'master' of https://github.com/lucasb243/ottomizeMaxLucas

--- src/components/ui/forms/RegisterForm.js
+++ src/components/ui/forms/RegisterForm.js
@@ -1,78 +1,87 @@
-import {Button, Modal} from 'react-bootstrap';
-import React, {useEffect, useState} from 'react';
-import {Col, Form, Row} from 'react-bootstrap';
+import { Button, Modal } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Col, Form, Row } from "react-bootstrap";
 function Registerform(props) {
-    const [isLoadingSubmit, setLoading] = useState(false);
-    useEffect(() => {
-        if (isLoadingSubmit) {
-            simulateNetworkRequest().then(() => {
-                setLoading(false);
-            });
-        }
-    }, [isLoadingSubmit]);
-    function handleSubmit(props){
-        setLoading(true);
-        props.onSubmit()
+  const [isLoadingSubmit, setLoading] = useState(false);
+  useEffect(() => {
+    if (isLoadingSubmit) {
+      simulateNetworkRequest().then(() => {
+        setLoading(false);
+      });
     }
-    const handleClick = () => handleSubmit(props);
-    function simulateNetworkRequest() {
-        return new Promise((resolve) => setTimeout(resolve, 2000));
-    }
-    return (
-        <>
-            <Form>
-                <Modal show={props.show} onHide={props.onClose} centered
-                aria-labelledby="contained-modal-title-vcenter">
-                    <Modal.Dialog className="m-0">
-                        <Modal.Header closeButton>
-                            <Modal.Title>Register</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                            <Form.Group controlId="formGridUserName">
-                                <Form.Label>Username</Form.Label>
-                                <Form.Control type="text" placeholder="Enter username"/>
-                            </Form.Group>
-                            <Form.Group controlId="formGridEmail">
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email"/>
-                            </Form.Group>
-                            <Row className="mb-3">
-                                <Form.Group as={Col}
-                                    controlId="formGridPassword">
-                                    <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" placeholder="Password"/>
-                                </Form.Group>
-                                <Form.Group as={Col}
-                                    controlId="formGridPasswordConfirm">
-                                    <Form.Label>Confirm Password</Form.Label>
-                                    <Form.Control type="password" placeholder="Repeat password"/>
-                                </Form.Group>
-                            </Row>
-                            <Form.Group className="mb-3" controlId="formGridAddress1">
-                                <Form.Label>Address</Form.Label>
-                                <Form.Control placeholder="1234 Main St"/>
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formGridAddress2">
-                                <Form.Label>Address 2</Form.Label>
-                                <Form.Control placeholder="Apartment, studio, or floor"/>
-                            </Form.Group>
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <Button variant="secondary" type="button"
-                                onClick={
-                                    props.onClose
-                                }
-                                size="lg">
-                                Cancel
-                            </Button>
-                            <Button variant="primary" type="submit" size="lg" disabled={isLoadingSubmit} onClick={ !isLoadingSubmit ? handleClick : null}> 
-                                {isLoadingSubmit ? 'Loading…' : 'Submit'} 
-                            </Button>
-                        </Modal.Footer>
-                    </Modal.Dialog>
-                </Modal>
-            </Form>
-        </>
-    )
+  }, [isLoadingSubmit]);
+  function handleSubmit(props) {
+    setLoading(true);
+    props.onSubmit();
+  }
+  const handleClick = () => handleSubmit(props);
+  function simulateNetworkRequest() {
+    return new Promise((resolve) => setTimeout(resolve, 2000));
+  }
+  return (
+    <>
+      <Form>
+        <Modal
+          show={props.show}
+          onHide={props.onClose}
+          centered
+          aria-labelledby="contained-modal-title-vcenter"
+        >
+          <Modal.Dialog className="m-0">
+            <Modal.Header closeButton>
+              <Modal.Title>Register</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form.Group controlId="formGridUserName">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="text" placeholder="Enter username" />
+              </Form.Group>
+              <Form.Group controlId="formGridEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="formGridPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group as={Col} controlId="formGridPasswordConfirm">
+                  <Form.Label>Confirm Password</Form.Label>
+                  <Form.Control type="password" placeholder="Repeat password" />
+                </Form.Group>
+              </Row>
+              <Form.Group className="mb-3" controlId="formGridAddress1">
+                <Form.Label>Address</Form.Label>
+                <Form.Control placeholder="1234 Main St" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formGridAddress2">
+                <Form.Label>Address 2</Form.Label>
+                <Form.Control placeholder="Apartment, studio, or floor" />
+              </Form.Group>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button
+                variant="secondary"
+                type="button"
+                onClick={props.onClose}
+                size="lg"
+              >
+                Cancel
+              </Button>
+              <Button
+                variant="primary"
+                type="submit"
+                size="lg"
+                disabled={isLoadingSubmit}
+                onClick={!isLoadingSubmit ? handleClick : null}
+              >
+                {isLoadingSubmit ? "Loading…" : "Submit"}
+              </Button>
+            </Modal.Footer>
+          </Modal.Dialog>
+        </Modal>
+      </Form>
+    </>
+  );
 }
 export default Registerform;
